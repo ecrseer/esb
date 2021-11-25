64a5c7af4b2efab059daad602209c8b158f460f0
ecrseer <juustino@live.com>
2021-11-25 20:47:12 -0300

    more notes

--- app/src/main/java/com/example/myapplication/model/NoteImagens.kt
+++ app/src/main/java/com/example/myapplication/model/NoteImagens.kt
@@ -4,13 +4,13 @@ object NoteImagens {
     var imgs  = mutableListOf(
         ImagemPesquisada("pg","nsei","olha eu    ","titulo2"),
         ImagemPesquisada("pg","nsei","olha eu com  ","titulo3"),
-        ImagemPesquisada("pg","nsei","olha eu com bone","titulo3"),
-        ImagemPesquisada("pg","nsei","olha eu sem bone","titulo3"),
-        ImagemPesquisada("pg","nsei","olha eu com bone","titulo3"),
-        ImagemPesquisada("pg","nsei","olha eu com bone","titulo3"),
-        ImagemPesquisada("pg","nsei","olha eu com bone","titulo3"),
-        ImagemPesquisada("pg","nsei","olha eu com bone","titulo3"),
-        ImagemPesquisada("pg","nsei","olha eu com bone","titulo3"),
+        ImagemPesquisada("pg","nsei","olha eu com bone","titulo4"),
+        ImagemPesquisada("pg","nsei","olha eu sem bone","titulo5"),
+        ImagemPesquisada("pg","nsei","olha eu com bone","titulo6"),
+        ImagemPesquisada("pg","nsei","olha eu com bone","titulo7"),
+        ImagemPesquisada("pg","nsei","olha eu com bone","titulo8"),
+        ImagemPesquisada("pg","nsei","olha eu com bone","titulo9"),
+        ImagemPesquisada("pg","nsei","olha eu com bone","titulo10"),
 
     )
     var imgsPeneiradas = mutableListOf<ImagemPesquisada>()
